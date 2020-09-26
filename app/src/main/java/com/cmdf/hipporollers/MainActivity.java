3e4b36fb2a9f0cc6acb2e78ceea772ae1daf7842
naadiya_a <49887062+naadiya-a@users.noreply.github.com>
2020-09-26 15:40:56 -0700

    splash screen, main menu, game view window

--- app/src/main/java/com/cmdf/hipporollers/MainActivity.java
+++ app/src/main/java/com/cmdf/hipporollers/MainActivity.java
@@ -9,12 +9,20 @@ import android.view.WindowManager;
 
 //import com.cmdf.hipporollers.Character.GameView;
 //import com.google.android.material.bottomnavigation.BottomNavigationView;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.navigation.NavController;
-//import androidx.navigation.Navigation;
-//import androidx.navigation.ui.AppBarConfiguration;
-//import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+//public class MainActivity extends AppCompatActivity {
 
 public class MainActivity extends Activity {
     @Override
@@ -50,24 +58,6 @@ public class MainActivity extends Activity {
 //                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 //        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 //        setContentView(new GameView(this));
-//    }
-
-
-// ORIGINAL!
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(navView, navController);
 //    }
 
 }

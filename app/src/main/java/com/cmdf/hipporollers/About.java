c848c381bbcff95c8f395eece2c7da0b41e2920c
naadiya_a <49887062+naadiya-a@users.noreply.github.com>
2020-09-26 22:56:43 -0700

    Linked menu to about page

--- app/src/main/java/com/cmdf/hipporollers/About.java
+++ app/src/main/java/com/cmdf/hipporollers/About.java
@@ -4,10 +4,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class About extends AppCompatActivity {
 
@@ -16,6 +18,39 @@ public class About extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+
+        ImageButton back = (ImageButton) findViewById(R.id.backBtn);
+        Button website = (Button) findViewById(R.id.website);
+        Button video = (Button) findViewById(R.id.video);
+
+        website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.rollahippo.org/";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://youtu.be/R0PfmYA9nY4";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(About.this, MainActivity.class));
+            }
+        });
     }
 
 //    public void openWebPage(String url) {
